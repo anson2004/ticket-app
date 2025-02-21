53c64d8c4676877de363f12896a050b796e139eb
yiqing.liang <yiqing.liang@hotmail.com>
2025-02-21 14:10:30 -0800

    add next auth

--- components/MainNav.tsx
+++ components/MainNav.tsx
@@ -1,21 +1,29 @@
-import Link from 'next/link'
-import React from 'react'
-import ToggleMode from './ToggleMode'
-import MainNavLinks from './MainNavLinks'
+import Link from "next/link";
+import React from "react";
+import ToggleMode from "./ToggleMode";
+import MainNavLinks from "./MainNavLinks";
+import { getServerSession } from "next-auth";
+import options from "@/app/api/auth/[...nextauth]/options";
 
-function MainNav() {
+async function MainNav() {
+  const session = await getServerSession(options);
+  console.log(session)
   return (
     <div className="flex justify-between items-center p-4">
       <MainNavLinks />
-      
-      <div className="flex  items-center gap-2">   
-          <Link href="/tickets">logout</Link>
-          <ToggleMode />
-          
+
+
+      <div className="flex  items-center gap-2">
+        {session ? (
+          <Link href="/api/auth/signout?callbackUrl=/">logout</Link>
+        ) : (
+          <Link href="/api/auth/signin">Signin</Link>
+        )}
+
+        <ToggleMode />
       </div>
     </div>
-  
-  )
+  );
 }
 
-export default MainNav
+export default MainNav;
